2952fa9a8d92d6a4bd0a68d24017941318174776
haidy <haidy>
2017-09-20 08:03:34 -0500

    Correciones

---
+++ assets/js/procedimental.js
@@ -0,0 +1,29 @@
+
+
+// Board Object
+function Board(row, col){
+    this.row = row;
+    this.column = column;   
+    this.spaces = [];    
+    this.gameOver = false;
+    this.spacesCleared = 0;
+    this.bombCount = 0;
+
+    this.click = function () {
+
+    }
+
+    this.render = function() {
+
+    }
+
+    this.explode = function() {
+   
+    }
+
+    this.clear = function () {
+
+    }
+
+}
+
